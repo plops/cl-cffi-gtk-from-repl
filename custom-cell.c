9a51814cedc3b9c8bf91a8c04a03ae73ac8d30c3
Martin Kielhorn <kielhorn.martin@gmail.com>
2014-11-15 17:27:42 +0100

    it is actually possible to call lambda functions!

--- custom-cell.c
+++ custom-cell.c
@@ -3,16 +3,25 @@
 
 int main(int argc, char**argv)
 {
-  GtkWidget *window;
+  GtkWidget *window, *label;
   gtk_init(&argc,&argv);
   window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   gtk_window_set_title(GTK_WINDOW(window),"hello world");
-  gtk_widget_show(window);
+  gtk_widget_set_size_request(window,200,100);
+  gtk_container_set_border_width(GTK_CONTAINER(window),10);
 
-  ({void _() {
-    printf("World!\n");
-  } (void (*)())_;})();
+  g_signal_connect(G_OBJECT(window),"destroy",
+		   G_CALLBACK({void _() { gtk_main_quit();  } (void (*)())_;}),NULL);
+  g_signal_connect(G_OBJECT(window),"delete-event",
+		   G_CALLBACK({gboolean _() { return FALSE;  } (void (*)())_;}),NULL);
 
+  label = gtk_label_new("Hello World");
+  gtk_label_set_selectable(GTK_LABEL(label),TRUE);
+
+  gtk_container_add(GTK_CONTAINER(window),label);
+  
+  gtk_widget_show_all(window);
+  
 
   gtk_main();
   

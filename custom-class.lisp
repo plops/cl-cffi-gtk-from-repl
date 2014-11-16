8ded9a830b16c0cdc3e15c1bade996c511e330f9
Martin Kielhorn <kielhorn.martin@gmail.com>
2014-11-16 23:40:16 +0100

    try to call g-type-register-static

--- custom-class.lisp
+++ custom-class.lisp
@@ -6,6 +6,8 @@
 
 (in-package :custom-class)
 
+
+
 ;; http://scentric.net/tutorial/sec-custom-cell-renderers.html
 #+nil
 (defcallback custom-cell-renderer-progress-finalize :void ((obj (g-object g-object-class)))
@@ -22,6 +24,33 @@
   ;; GTK_CELL_RENDERER_CLASS
   (let ((parent-class (g-type-class-peek-parent class) :parent-class))
     (setf (foreign-slot-value class (g-object g-object-class) :finalize) )))
+
+(defcstruct _my-ip-address (entry (g-object gtk-entry)))
+(defcstruct _my-ip-address-class (parent-class (g-object gtk-entry-c)))
+(defcallback my-ip-address-class-init :void ((klass :pointer) (data (g-object gpointer)))
+  ;; REGISTER signals
+  )
+(defcallback my-ip-address-init :void ((ip-address :pointer))
+  ;; REGISTER signals
+  )
+;;(foreign-type-size '_my-ip-address-class)
+#+nil
+(let ((entry-type 0))
+  (defun my-ip-address-get-type ()
+    (when (= 0 entry-type)
+      (cffi:with-foreign-object (info '(:struct gobject:g-type-info))
+	(setf (foreign-slot-value info :uint16 :class-size) (foreign-type-size '_my-ip-address-class)
+	      (foreign-slot-value info :pointer :class-init-fn) my-ip-address-class-init
+	      (foreign-slot-value info :pointer :instance-init-fn) my-ip-address-init)
+	      )
+	(g-type-register-static (g-type-from-name "GtkEntry")
+				"MyIPAddress"
+				info
+				0)))
+    entry-type))
+
+;; (g-object-new (my-ip-address-get-type)
+;;(foreign-slot-offset )
 
 
 #+nil
@@ -40,6 +69,11 @@
 				"CustomCellRenderProgress"
 				info 0)))
      cell-progress-type)))
+#+nil
+(gobject:g-object  'gtk-entry)
+
+#+nil
+(g-type-from-name "GtkEntry")
 #+inl
 (defparameter *bla* (cffi:defcstruct custom_class (parent-class (g-object gtk-cell-renderer-class))))
 #+nil

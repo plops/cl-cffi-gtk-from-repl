83690c706783d3d6a742ba395c37d5e01392bf0d
Martin Kielhorn <kielhorn.martin@gmail.com>
2014-12-09 10:23:19 +0100

    C compiles

--- callb/callb.c
+++ callb/callb.c
@@ -1,15 +1,38 @@
-
-typedef struct obj obj;
-struct obj {
-  int set(obj*o,int val);
+#include <malloc.h>
+typedef struct T T;
+struct T {
+  int (*set)(T*o,int val);
+  int (*callb)(T*o,void*data);
   int val;
 };
 
-int set(obj*o,int val)
+static int set(T*o,int val)
 {
   return o->val = val;
 }
 
-int init()
+enum {N_TYPES=10};
+T*types[N_TYPES];
+
+int regist(int class_id,void*callb)
 {
+  if(class_id<N_TYPES){
+    T* o = malloc(sizeof(T));
+    o->set = &set;
+    o->callb = callb;
+    types[class_id]=o;
+    return 0;
+  } else {
+    return -1;
+  }
+}
+
+int unregist(int class_id)
+{
+  if(types[class_id]==NULL)
+    return -1;
+ 
+  free(types[class_id]);
+  types[class_id]=NULL;
+  return 0;
 }
